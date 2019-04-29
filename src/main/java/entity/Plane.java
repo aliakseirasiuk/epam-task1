75fe086785e70dadc1619425cb19489967729e18
aliakseirasiuk <aliaksei.rasiuk@gmail.com>
2019-04-29 23:45:50 +0300

    adding 2 entities: Plane and Company

---
+++ src/main/java/entity/Plane.java
@@ -0,0 +1,13 @@
+package entity;
+
+public class Plane {
+
+    private int weightCapacity;
+
+    private int cargoCapacity;
+
+    private int currentFuelLoad;
+
+    private int maxFuelLoad;
+
+}
